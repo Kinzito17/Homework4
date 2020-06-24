417f5b6d720b7ef4bfd6c1548fb9bb13ac4e3440
Matthew Kinzle <matt.kinzle@gmail.com>
2020-06-23 22:37:04 -0500

    not so final

---
+++ questions.js
@@ -0,0 +1,28 @@
+var questions = [
+   {
+        question: "1. Commonly used data types DO NOT include:",
+        choices: ["Strings", "Booleans", "Alerts", "Numbers"],
+        answer: "Alerts"
+    },
+    {
+        question: "2. The condition in an if / else statement is enflosed within ____.",
+        choices: ["Quotes", "Curly Brackets", "Parenthesis", "Square Brackets"],
+        answer: "Curly Brackets"
+    },
+    {
+        question: "3. Arrays in JavaScript can be used to store ____.",
+        choices: ["Numbers and Strings", "Other Arrays", "Booleans", "All of the Above"],
+        answer: "All of the Above"
+    },
+    {
+        question: "4. String values must be enclosed within ____ when being assigned to variables.",
+        choices: ["Commas", "Curly Brackets", "Quotes", "Parenthesis"],
+        answer: "Quotes"
+    },
+    {
+        question: "5. A very useful tool used during development and debugging for printing content to the debugger is:",
+        choices: ["JavaScript", "Terminal / Bash", "For Loops", "Console.log"],
+        answer: "Console.log"
+    }
+]
+
--- script.js
+++ script.js
@@ -1,46 +1,122 @@
-var secondsLeft
-var score = secondsLeft
-var countDown = document.querySelector("timer")
+//Quiz Questions
+var questions = [
+    {
+        question: "1. Commonly used data types DO NOT include:",
+        choices: ["Strings", "Booleans", "Alerts", "Numbers"],
+        answer: "Alerts"
+    },
+    {
+        question: "2. The condition in an if / else statement is enflosed within ____.",
+        choices: ["Quotes", "Curly Brackets", "Parenthesis", "Square Brackets"],
+        answer: "Curly Brackets"
+    },
+    {
+        question: "3. Arrays in JavaScript can be used to store ____.",
+        choices: ["Numbers and Strings", "Other Arrays", "Booleans", "All of the Above"],
+        answer: "All of the Above"
+    },
+    {
+        question: "4. String values must be enclosed within ____ when being assigned to variables.",
+        choices: ["Commas", "Curly Brackets", "Quotes", "Parenthesis"],
+        answer: "Quotes"
+    },
+    {
+        question: "5. A very useful tool used during development and debugging for printing content to the debugger is:",
+        choices: ["JavaScript", "Terminal / Bash", "For Loops", "Console.log"],
+        answer: "Console.log"
+    }
+]
 
-var seconds = 75
+//Variables
+
+//start the quiz
+var startButton = document.querySelector("#start-quiz");
+//starts the quiz everytime with 75 seconds
+var countdown = document.querySelector("#countdown");
+//Div where questions will populate
+var questionsHtml = document.querySelector("#questions")
+//Not sure if I need this but just to be safe
+var container = document.querySelector("#container")
+//used to create new list element
+var ulEl = document.createElement("ul")
+//score starts as 0
+var score = 0;
+//index reference for rendering questions
+var questionsIndex = 0;
+
+//Variables for countdown
+//user will start with 75 seconds
+var secondsAmount = 75;
+//the function will call this to start
+var startSecs = 0;
+//seconds taken away for question answered wrong
+var missed = 10;
 
 
-functions setTimer() {
-    var timerInterval = setInterval(function() {
-        if (seconds < 0) {
-            clearInterval(x);
-            document.getElementById("timer").innerHTML = "Your time is up!";
+//start Quiz
+startButton.addEventListener("click", function () {
+    if (startSecs === 0) {
+        startSecs = setInterval(function () {
+            secondsAmount--;
+            countdown.textContent = "Time: " + secondsAmount;
+
+            if (secondsAmount <= 0) {
+                clearInterval(startSecs);
+                quizOver();
+                countdown.textcontent = "Quiz is over";
+            }
+        }, 1000);
+    }
+    render(questionsIndex);
+});
+
+//for loop pulls question and answer objects from array
+function render(questionsIndex) {
+    questionsHtml.innerHTML = "";
+    ulEl.innerHTML = "";
+    for (var i = 0; i < questions.length; i++) {
+        var quizQuestions = questions[questionsIndex].question;
+        var ansChoices = questions[questionsIndex].choices;
+        questionsHtml.textContent = quizQuestions;
+    }
+
+    // creates new li when the answers are presented
+    ansChoices.forEach(function (newLi) {
+        var listItem = document.createElement("li");
+        listItem.textContent = newLi;
+        questionsHtml.appendChild(ulEl);
+        ulEl.appendChild(listItem);
+        listItem.addEventListener("click", (compare));
+    })
+};
+
+function compare(event) {
+    var element = event.target;
+    if (element.matches("li")) {
+        var newDiv = document.createElement("div");
+        newDiv.setAttribute("id", "newDiv")
+        if (element.textContent == questions[questionsIndex].answer) {
+            newDiv.textContent = "Correct";
+        } else {
+            startTime = secondsAmount - missed;
+            newDiv.textContent = "Wrong";
+        }
+    }
+    questionsIndex++;
+    if (questionsIndex >= questions.length) {
+        quizOver();
+        newDiv.textContent = "All Done!"
+    } else {
+        render(questionsIndex);
+
+    }
+    questionsHtml.appendChild(newDiv)
+}
+
+function quizOver() {
+    questionsHtml.innerHTML = "";
+    countdown.innerHTML = "";
+}
 
 
-
-function startQuiz() {
-
-
-// Store data
-var score = "";
-localStorage.setItem('', score);
-
-// Get data
-var score = localStorage.getItem('myDataKey');
-
-
-
-
-function startTimer(duration, display) {
-
-var timeleft = 75;
-var countDown = setInterval(function() {
-    var timer = duration, seconds;
-
-  if(timeleft <= 0){
-    clearInterval(countDown);
-    document.getElementById("countdown").innerHTML = "Finished";
-  } else {
-    document.getElementById("countdown").innerHTML = timeleft + " seconds remaining";
-  }
-  timeleft -= 1;
-}, 1000);
-
-todoForm.addEventListener("submit", function(event) {
-    event.preventDefault();
 
